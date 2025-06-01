9cf7ec8e14d5d28e5cba74a98a5b40f965444b26
Md. Ammar Hossain <140334526+gitForAmmar11@users.noreply.github.com>
2025-06-02 02:38:26 +0600

    Add files via upload

---
+++ Final Question Solve/213 Q Solve/Question1B.java
@@ -0,0 +1,10 @@
+package FInalQuestionSolve.Fall2021;
+
+public class Question1B {
+    static int x;
+    final int y = 213;
+
+    public static void main(String[] args) {
+        x = 50;
+    }
+}
